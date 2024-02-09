4ddf396af94a9431a95960d60bc7e645f794646c
rt10000 <rt10000@yandex.ru>
2024-02-09 19:24:54 +0300

    Создаем репозиторий проекта

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnFind = findViewById<Button>(R.id.btnFind)
+
+//        val buttonFindListener:  View.OnClickListener = object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                Toast.makeText(this@MainActivity,"Нажата кнопка поиск", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+        btnFind.setOnClickListener {
+            val findIntent = Intent(this, FindActivity::class.java)
+            startActivity(findIntent)
+        }
+
+       val btnMedia =  findViewById<Button>(R.id.btnMedia)
+       //btnMedia.setOnClickListener {Toast.makeText(this@MainActivity,"Нажата кнопка медиатека", Toast.LENGTH_SHORT).show()}
+        btnMedia.setOnClickListener {
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+       val btnSetting = findViewById<Button>(R.id.btnSetting)
+       btnSetting.setOnClickListener {
+           val settingIntent = Intent(this, SettingsActivity::class.java)
+           startActivity(settingIntent)
+       }
     }
 }
