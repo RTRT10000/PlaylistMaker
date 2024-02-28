3c836c392cf17de553ed5db3ce893a5d69fcff66
rt10000 <rt10000@yandex.ru>
2024-02-28 18:03:46 +0300

    Создаем репозиторий проекта

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,10 +2,70 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 
 class SearchActivity : AppCompatActivity() {
+
+   companion object {
+       const val INPUT_TEXT = "INPUT_TEXT"
+       const val TEXT_DEF = ""
+   }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                inputText = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //empty
+            }
+        }
+        inputEditText.addTextChangedListener(searchTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_TEXT, inputText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        super.onRestoreInstanceState(savedInstanceState)
+        inputText = savedInstanceState.getString(INPUT_TEXT, TEXT_DEF)
+        inputEditText.setText(inputText)
+
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
+
+private var inputText: String = ""
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -8,6 +12,34 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        val tvWriteSupport = findViewById<TextView>(R.id.tvWriteSupport)
+        tvWriteSupport.setOnClickListener {
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val writeSupportIntent = Intent(Intent.ACTION_SENDTO)
+            writeSupportIntent.data = Uri.parse("mailto:")
+            writeSupportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("rt10000@ya.ru"))
+            writeSupportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            writeSupportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(writeSupportIntent)
 
+            Toast.makeText(this@SettingsActivity, "Здесь какой-то текст", Toast.LENGTH_SHORT).show()
+        }
+
+        val tvAgreement = findViewById<TextView>(R.id.tvAgreement)
+        tvAgreement.setOnClickListener {
+            val  url="https://yandex.ru/legal/practicum_offer/"
+            val agreementIntent = Intent(Intent.ACTION_VIEW)
+            agreementIntent.data = Uri.parse(url)
+            startActivity(agreementIntent)
+        }
+
+        val tvShare = findViewById<TextView>(R.id.tvShare)
+        tvShare.setOnClickListener {
+          val shareIntent = Intent(Intent.ACTION_SEND)
+          shareIntent.type = "text/plain"
+          shareIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/?utm_source=yandex-desktop-all&utm_medium=regular&utm_campaign=ya-ru_popup-all-services-desktop")
+          startActivity(Intent.createChooser(shareIntent,"Share with"))
+        }
     }
 }
