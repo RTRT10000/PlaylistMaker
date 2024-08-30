c9750889521b5c9a19f0960fe1aca165e884b310
rt10000 <rt10000@yandex.ru>
2024-08-30 20:46:34 +0300

    Спринт 13. Макет экрана плеера

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -6,4 +6,8 @@ data class Track(
     val trackTimeMillis: String,
     val artworkUrl100: String,
     val trackId: Int,
+    val collectionName: String,
+    val releaseDate: Int,
+    val primaryGenreName: String,
+    val country: String,
 )
